293f5aab369fe021470e6b8a495e33dc53f528c9
Arie Maljers <ariemaljers@gmail.com>
2023-08-30 17:05:17 +1000

    added PrintandHide function

--- Assets/PrintAndHide.cs
+++ Assets/PrintAndHide.cs
@@ -2,13 +2,15 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ConsolePrint : MonoBehaviour
+public class PrintAndHide : MonoBehaviour
 {
     private int i;
+    public int j;
     public Renderer rend;
     void Start()
     {
         i = 3;
+        j = Random.Range(150,251);
     }
 
     // Update is called once per frame
@@ -16,5 +18,15 @@ public class ConsolePrint : MonoBehaviour
     {
         i += 1;
         Debug.Log(gameObject.name + ":" + i);
+
+        if (gameObject.tag == "Red" && i == 100)
+        {
+            gameObject.SetActive(false);
+        }
+
+        if (gameObject.tag == "Blue" && i == j)
+        {
+            rend.enabled = false;
+        }
     }
 }
